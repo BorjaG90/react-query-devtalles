12ac7291da0574dff20ebc6f86e3cde58b7dc5f4
BorjaG90 <borjag90dev@gmail.com>
2023-08-17 12:48:13 +0200

    feat(App): add useRandom hook #1

--- 01-cripto-random/src/App.tsx
+++ 01-cripto-random/src/App.tsx
@@ -1,19 +1,8 @@
-import { useQuery } from '@tanstack/react-query';
+import { useRandom } from './hooks/useRandom';
 import './App.css';
 
-const getRandomNumberFromAPI = async():Promise<number> => {
-  const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
-  const numberString = await res.text();
-
-  // throw new Error('Auxilio!');
-  return +numberString;
-}
-
 export const App = () => {
-  const query = useQuery(
-    ['randomNumber'], //cache
-    getRandomNumberFromAPI
-  );
+  const query = useRandom();
 
   return (
     <div className="App App-header">
