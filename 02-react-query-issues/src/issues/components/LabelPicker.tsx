a620c1415d3559ad2355fdc99066861393f0ecd2
BorjaG90 <borjag90dev@gmail.com>
2023-08-20 13:07:59 +0200

    feat(*): use Axios to fetch the labels & interface #3

--- 02-react-query-issues/src/issues/components/LabelPicker.tsx
+++ 02-react-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,14 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
 
-const getLabels = async () => {
-  const res = await fetch("https://api.github.com/repos/facebook/react/labels");
-  const data = await res.json();
-  console.log(data);
-  return data;
-};
+// const getLabels = async () => {
+//   const res = await fetch("https://api.github.com/repos/facebook/react/labels");
+//   const data = await res.json();
+//   console.log(data);
+//   return data;
+// };
+
+const getLabels = async():Promise<Label[]> => {
+	const { data } = await githubApi.get<Label[]>('/labels');
+	return data;
+	};
 
 export const LabelPicker = () => {
-  const labelsQuery = useQuery(["labels"], getLabels);
+  const labelsQuery = useQuery(
+		["labels"],
+		getLabels
+	);
+
   return (
     <div>
       <span
