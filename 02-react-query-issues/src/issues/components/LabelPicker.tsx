f166556cc80f5681eb43d34b48019d04a7aef2c6
BorjaG90 <borjag90dev@gmail.com>
2023-08-20 13:19:55 +0200

    feat(*): add useLabels & useQuery #3

--- 02-react-query-issues/src/issues/components/LabelPicker.tsx
+++ 02-react-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,33 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
-import { githubApi } from "../../api/githubApi";
-import { Label } from "../interfaces/label";
-
-// const getLabels = async () => {
-//   const res = await fetch("https://api.github.com/repos/facebook/react/labels");
-//   const data = await res.json();
-//   console.log(data);
-//   return data;
-// };
-
-const getLabels = async():Promise<Label[]> => {
-	const { data } = await githubApi.get<Label[]>('/labels');
-	return data;
-	};
+import { useLabels } from "../hooks/useLabels";
 
 export const LabelPicker = () => {
-  const labelsQuery = useQuery(
-		["labels"],
-		getLabels
-	);
+  const labelsQuery = useLabels();
+
+	if ( labelsQuery.isLoading ) //! porque isLoading y no isFetching
+		return (<h1>Loading...</h1>);
 
   return (
     <div>
-      <span
-        className="badge rounded-pill m-1 label-picker"
-        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
-      >
-        Primary
-      </span>
+			{
+				labelsQuery.data?.map(label => (
+					<span
+						key={label.id}
+						className="badge rounded-pill m-1 label-picker"
+						style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
+					>{label.name}</span>
+				))
+			}
     </div>
   );
 };
