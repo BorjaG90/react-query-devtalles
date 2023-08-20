04b8adb280fa85e96d0c7dc2209481acd223c559
BorjaG90 <borjag90dev@gmail.com>
2023-08-20 13:43:19 +0200

    feat(useLabels): add initialData & placeholderData #3

--- 02-react-query-issues/src/issues/hooks/useLabels.tsx
+++ 02-react-query-issues/src/issues/hooks/useLabels.tsx
@@ -12,12 +12,32 @@ const getLabels = async():Promise<Label[]> => {
 
 export const useLabels = () => {
   const labelsQuery = useQuery(
-		['labels'],
-		getLabels,
+    ["labels"],
+    getLabels,
     {
-      staleTime: 1000 * 60 * 60
+      staleTime: 1000 * 60 * 60,
+      // placeholderData: [ // No se considera fresh, despues del staleTime realiza petición
+      // ],
+      initialData: [ // Se considera fresh, no realiza petición hasta staleTime
+        {
+          id: 725156255,
+          node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+          name: "good first issue (taken)",
+          color: "b60205",
+          default: false,
+        },
+        {
+          id: 717031390,
+          node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+          url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+          name: "good first issue",
+          color: "6ce26a",
+          default: true,
+        },
+      ],
     }
-	);
+  );
 
   return labelsQuery;
 }
