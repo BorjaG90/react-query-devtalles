02d2cde50eecfe5f867d65f4363506bc94506f45
BorjaG90 <borjag90dev@gmail.com>
2023-08-21 11:47:34 +0200

    feat(githubApi): add header #5

--- 02-react-query-issues/src/api/githubApi.ts
+++ 02-react-query-issues/src/api/githubApi.ts
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
-  headers: {}
+  headers: {
+    Authorization: 'Bearer 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'
+  }
 });
