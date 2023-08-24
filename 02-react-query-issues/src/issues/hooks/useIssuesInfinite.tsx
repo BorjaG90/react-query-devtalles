4914b678cd535a27df26131f69e0958b52939d88
BorjaG90 <borjag90dev@gmail.com>
2023-08-24 19:03:34 +0200

    feat(useIssuesInfinite): add infinite scroll custom hook #11

--- 02-react-query-issues/src/issues/hooks/useIssuesInfinite.tsx
+++ 02-react-query-issues/src/issues/hooks/useIssuesInfinite.tsx
@@ -1,5 +1,42 @@
-export const useIssuesInfinite = () => {
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { Issue, State } from '../interfaces';
+import { sleep } from '../../helpers';
+import { githubApi } from '../../api/githubApi';
+
+interface Props {
+  state?: State;
+  labels: string[];
+  page?: number;
+}
+
+const getIssues = async({labels, state, page = 1}: Props):Promise<Issue[]> => {
+  await sleep(2);
+
+  const params = new URLSearchParams();
+
+  if (state) params.append('state', state);
+
+  if (labels.length > 0) {
+    const labelString = labels.join(',');
+    params.append('labels', labelString);
+  }
+
+  params.append('page', page.toString());
+  params.append('per_page', '5');
+
+  const { data } = await githubApi.get<Issue[]>('/issues', {params});
+  return data;
+}
+
+export const useIssuesInfinite = ({state, labels}: Props) => {
+  const issuesQuery = useInfiniteQuery(
+    ['issues', 'infinite', {state, labels, page: 1}],
+    (data) => getIssues(data),
+    {
+      //Todo: getNextPageParam()
+    }
+  );
   return {
-    
+    issuesQuery
   }
 }
