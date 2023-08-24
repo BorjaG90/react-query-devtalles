0d5a13638db5ee5b0bd774bac083edfa1d2aaf92
BorjaG90 <borjag90dev@gmail.com>
2023-08-24 18:42:29 +0200

    feat(*): add Next & Prevoius page #11

--- 02-react-query-issues/src/issues/hooks/useIssues.tsx
+++ 02-react-query-issues/src/issues/hooks/useIssues.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+
 import { githubApi } from '../../api/githubApi';
 import { Issue, State } from '../interfaces';
 import { sleep } from '../../helpers';
@@ -6,9 +8,10 @@ import { sleep } from '../../helpers';
 interface Props {
   state?: State;
   labels: string[];
+  page?: number;
 }
 
-const getIssues = async(labels: string[] = [], state?: State):Promise<Issue[]> => {
+const getIssues = async({labels, state, page = 1}: Props):Promise<Issue[]> => {
   await sleep(2);
 
   const params = new URLSearchParams();
@@ -20,7 +23,7 @@ const getIssues = async(labels: string[] = [], state?: State):Promise<Issue[]> =
     params.append('labels', labelString);
   }
 
-  params.append('page', '1');
+  params.append('page', page.toString());
   params.append('per_page', '5');
 
   const { data } = await githubApi.get<Issue[]>('/issues', {params});
@@ -28,12 +31,32 @@ const getIssues = async(labels: string[] = [], state?: State):Promise<Issue[]> =
 }
 
 export const useIssues = ({state, labels}: Props) => {
+  const [page, setPage] = useState(1);
+
   const issuesQuery = useQuery(
-    ['issues', {state, labels}],
-    () => getIssues(labels, state)
+    ['issues', {state, labels, page}],
+    () => getIssues({labels, state, page})
   );
 
+  const nextPage = () => {
+    if ( issuesQuery.data?.length === 0 ) return;
+
+    setPage(page + 1);
+  }
+
+  const previousPage = () => {
+    if ( page > 1 ) setPage(page -1);
+  }
+
   return {
+    // Properties
     issuesQuery,
+
+    // Getter
+    page,
+
+    // Methods
+    nextPage,
+    previousPage
   };
 }
--- 02-react-query-issues/src/issues/views/ListView.tsx
+++ 02-react-query-issues/src/issues/views/ListView.tsx
@@ -12,7 +12,7 @@ export const ListView = () => {
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
   const [state, setState] = useState<State>();
   
-  const { issuesQuery } = useIssues({state, labels: selectedLabels});
+  const { issuesQuery, page, nextPage, previousPage } = useIssues({state, labels: selectedLabels});
 
   const onLabelChange = (labelName:string) => {
     ( selectedLabels.includes(labelName) )
@@ -37,9 +37,15 @@ export const ListView = () => {
         }
 
         <div className='d-flex mt-2 justify-content-between align-items-center'>
-          <button className='btn btn-outline-primary'>Prev</button>
-          <span>123</span>
-          <button className='btn btn-outline-primary'>Next</button>
+          <button
+            className='btn btn-outline-primary'
+            onClick={ previousPage }
+          >Prev</button>
+          <span>{ page }</span>
+          <button
+            className='btn btn-outline-primary'
+            onClick={ nextPage }
+          >Next</button>
         </div>
       </div>
       
