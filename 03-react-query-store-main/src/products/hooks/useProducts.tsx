49d27826a54d42e0b112619141c42c0673360e02
BorjaG90 <borjag90dev@gmail.com>
2023-10-27 14:54:06 +0200

    feat(*): modify product list #14

---
+++ 03-react-query-store-main/src/products/hooks/useProducts.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query"
+import { productActions } from ".."
+
+interface Options {
+  filterKey?: string
+}
+
+export const useProducts = ({filterKey}: Options) => {
+  const { isLoading, isError, error, data: products = [], isFetching } = useQuery(
+    ['products', { filterKey }],
+    () => productActions.getProducts({filterKey}),
+    {
+      staleTime: 1000,
+    }
+  );
+
+  return {
+    error,
+    isError,
+    isFetching,
+    isLoading,
+    products
+  }
+}
