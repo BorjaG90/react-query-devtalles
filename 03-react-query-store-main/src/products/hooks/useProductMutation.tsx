ef490fa0ae369b9cf2e34d725e174f2fbb5c2986
BorjaG90 <borjag90dev@gmail.com>
2023-10-28 12:49:40 +0200

    feat(useProductMutation): add optimistic update #13

--- 03-react-query-store-main/src/products/hooks/useProductMutation.tsx
+++ 03-react-query-store-main/src/products/hooks/useProductMutation.tsx
@@ -7,13 +7,33 @@ export const useProductMutation = () => {
 
   const mutation = useMutation({
     mutationFn: productActions.createProduct,
+
+    onMutate: (product) => {
+      console.log('Mutando - Optimistic update');
+
+      //! Opcional hacer la invalidación de query
+      // Optimistic product
+      const optimisticProduct = { id: Math.random(), ...product };
+      console.log(optimisticProduct);
+      
+      // Almacenar el producto en el cache del query client
+      queryClient.setQueryData<Product[]>(
+        ['products', { filterKey: product.category}],
+        (old) => {
+          if ( !old ) return [optimisticProduct];
+
+          return [...old, optimisticProduct];
+        }
+      );
+    },
+
     onSuccess: (data) => {
       /** Invalidate query */
       // queryClient.invalidateQueries(
       //   ['products', {'filterKey': data.category}]
       // );
 
-      /** No invalidate query */
+      /** Avoid invalidate query */
       queryClient.setQueryData<Product[]>(
         ['products', { filterKey: data.category }],
         (old) => {
